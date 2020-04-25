d10e93041135edb46ab095427e47715a9b9b72da
Igo Jeferson Barros Coelho <igojeferson.coelho@gmail.com>
2020-04-25 18:39:04 -0300

    - unused Import fix

--- kafka-consumer-elasticsearch/src/main/java/com/github/jeferson/kafka/ElasticSearchConsumer.java
+++ kafka-consumer-elasticsearch/src/main/java/com/github/jeferson/kafka/ElasticSearchConsumer.java
@@ -15,7 +15,6 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
